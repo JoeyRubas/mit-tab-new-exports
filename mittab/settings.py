e9181a9276747e8d27575d9a9b48683947675c5d
JoeyRubas <joeymrubas@gmail.com>
2025-03-01 09:39:06 -0500

    try white noise

--- mittab/settings.py
+++ mittab/settings.py
@@ -15,7 +15,7 @@ ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
-INSTALLED_APPS = ("django.contrib.admin", "django.contrib.auth",
+INSTALLED_APPS = ("whitenoise.runserver_nostatic", "django.contrib.admin", "django.contrib.auth",
                   "django.contrib.contenttypes", "django.contrib.sessions",
                   "django.contrib.messages", "django.contrib.staticfiles",
                   "mittab.apps.tab", "raven.contrib.django.raven_compat",
