43c639b2bc28a4f634fd7c3949e6a07ed72dcbb1
JoeyRubas <joeymrubas@gmail.com>
2025-03-06 19:07:07 -0500

    remove debug only option

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const BundleTracker = require("webpack-bundle-tracker");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const TerserPlugin = require("terser-webpack-plugin");
 const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
-const  BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+//const  BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 module.exports = {
   context: __dirname,
@@ -65,7 +65,7 @@ module.exports = {
 
   plugins: [
     new BundleTracker({ filename: "./webpack-stats.json" }),
-    new BundleAnalyzerPlugin(),
+    //new BundleAnalyzerPlugin(),
     new MiniCssExtractPlugin({
       filename: "[name].css",
       chunkFilename: "[id].css",
